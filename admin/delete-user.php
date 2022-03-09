bce7af2798f9c2965799e16bc172c59bc665f538
khanmohsin123 <fabulousmind345@gmail.com>
2022-03-09 14:12:58 +0500

    first commit

---
+++ admin/delete-user.php
@@ -0,0 +1,21 @@
+<?php
+if($_SESSION["user_role"] == '0')
+{
+     header("location: {$hostname}/admin/post.php");
+}
+
+include "config.php";
+$userid = $_GET['id'];
+$sql = "DELETE FROM user WHERE user_id = {$userid}";
+
+if(mysqli_query($conn, $sql))
+{
+    header("location : {$hostname}/admin/users.php");
+}
+else
+{
+    echo "<p style='color:red;margin: 10px 0;'>Can\'t delete the user record</p>";
+}
+mysqli_close($conn);
+
+?>
